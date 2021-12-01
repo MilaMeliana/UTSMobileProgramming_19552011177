dd1f58578b45f4e8b19d80f9c8af232eca368fcd
Mila Meliana <milameliana385@gmail.com>
2021-12-01 13:44:50 +0700

    first commit

--- lib/Data/data_19552011177.dart
+++ lib/Data/data_19552011177.dart
@@ -1,46 +1,46 @@
 class Produk {
   final String? cameraName;
-  final String? image;
   final num? price;
   final int? color;
+  final String? image;
 
   Produk({
     this.cameraName,
-    this.image,
     this.price,
     this.color,
+    this.image,
   });
 
   List<Produk> get listCamera => [
         Produk(
           cameraName: 'Mini Mint 7+',
-          image: 'images/InstaxMini7plus_Mint_R.png',
           price: 49.90,
           color: 0xff70b1a1,
+          image: 'images/InstaxMini7plus_Mint_R.png',
         ),
         Produk(
           cameraName: 'Mini Blue 7+',
-          image: 'images/InstaxMini7plus_Blue_R.png',
           price: 50.90,
           color: 0xff77a0c6,
+          image: 'images/InstaxMini7plus_Blue_R.png',
         ),
         Produk(
           cameraName: 'Mini Choral 7+',
-          image: 'images/InstaxMini7plus_Choral_R.png',
           price: 51.90,
           color: 0xffb0463c,
+          image: 'images/InstaxMini7plus_Choral_R.png',
         ),
         Produk(
           cameraName: 'Mini Pink 7+',
-          image: 'images/InstaxMini7plus_Pink_R.png',
           price: 52.90,
           color: 0xfffcf9496,
+          image: 'images/InstaxMini7plus_Pink_R.png',
         ),
         Produk(
           cameraName: 'Mini Lavender 7+',
-          image: 'images/InstaxMini7plus_Lavender_R.png',
           price: 53.90,
           color: 0xff855f8c,
+          image: 'images/InstaxMini7plus_Lavender_R.png',
         ),
       ];
 }
